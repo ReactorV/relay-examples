4284e4ba662bbc28b977c9732398735e50dbe1a8
Vadzim Voitkus <vvoitkus@gmail.com>
2023-05-26 19:40:26 +0200

    add useLazyLoadQuery

--- newsfeed/src/components/Newsfeed.tsx
+++ newsfeed/src/components/Newsfeed.tsx
@@ -1,24 +1,40 @@
 import * as React from "react";
+import { graphql } from 'relay-runtime';
+import { useLazyLoadQuery } from "react-relay";
+
+import type { NewsfeedQuery as NewsfeedQueryType } from './__generated__/NewsfeedQuery.graphql';
 import Story from "./Story";
 
-export default function Newsfeed() {
-  const story = {
-    title: "Placeholder Story",
-    summary: "Placeholder data, to be replaced with data fetched via GraphQL",
-    poster: {
-      name: "Placeholder Person",
-      profilePicture: {
-        url: "/assets/cat_avatar.png",
-      },
-    },
-    thumbnail: {
-      url: "/assets/placeholder.jpeg",
-    },
-  };
+const NewsfeedQuery = graphql`
+  query NewsfeedQuery {
+    topStory {
+      title
+      summary
+      poster {
+        name
+        profilePicture {
+          url
+        }
+      }
+      thumbnail {
+        url
+      }
+    }
+  }
+`;
 
+// eslint-disable-next-line no-empty-pattern
+export default function Newsfeed({}) {
+  const data = useLazyLoadQuery<NewsfeedQueryType>(
+      NewsfeedQuery,
+      {},
+  );
+
+  const story = data.topStory;
+  // As before:
   return (
-    <div className="newsfeed">
-      <Story story={story} />
-    </div>
+      <div className="newsfeed">
+        <Story story={story} />
+      </div>
   );
 }
